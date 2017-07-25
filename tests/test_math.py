4154fbd4eda2cf21eb8f63df5287a15b800c1244
Viswanath Pasumarthi <pasumart@buffalo.edu>
2017-07-25 15:04:57 -0400

    Successfully passed tests for mult function

--- tests/test_math.py
+++ tests/test_math.py
@@ -8,3 +8,7 @@ import pytest
 def test_add():
     assert ef.math.add(3, 4) == 7
     assert ef.math.add(4, 4) == 8
+
+def test_mult():
+    assert ef.math.mult(2, 5) == 10
+    assert ef.mult(4, 3) == 12
